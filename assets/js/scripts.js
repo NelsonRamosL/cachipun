37e1445fde84f380b3f8bbae7c0e1a18d9cafdaf
nNlson rRmos Lopez <jerunel@hotmail.com>
2021-07-06 22:43:39 -0400

    actualizado y agregado efecto tiempo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,18 +5,27 @@ console.log("entramos en js");
 
 
 //   Cambiar color mouse pasa sobre y cuando sale de posicion buelve a color blanco
-$( ".fas" ).mouseover(function() {
+$(".fas").mouseover(function() {
     $(this).css('background-color','red');
   });
 
-$( ".fas" ).mouseleave(function() {
+$(".fas").mouseleave(function() {
   $(this).css('background-color','white');
   });
 
 
 
-  $( ".fas" ).click(function() {
+  $(".fas").click(function() {
     $(this).css('background-color','green');
+
+    document.getElementById("tiempo").style.visibility = "visible";
+    console.log("aparecer");
+    // $("#tiempo").show();
+    setTimeout(function(){
+        $("#tiempo").css('visibility','hidden');
+        console.log("desaparecer")
+    },5000); 
+    
   });
 
 

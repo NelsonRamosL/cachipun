0031fe2593fa4baef3b86e544209fe07a8389c6a
nNlson rRmos Lopez <jerunel@hotmail.com>
2021-07-06 21:39:26 -0400

    actualizacion

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,23 @@
 $(document).ready(function () {
 
-
- 
-
 console.log("entramos en js");
+
+
+
+//   Cambiar color mouse pasa sobre y cuando sale de posicion buelve a color blanco
+$( ".fas" ).mouseover(function() {
+    $(this).css('background-color','red');
+  });
+
+$( ".fas" ).mouseleave(function() {
+  $(this).css('background-color','white');
+  });
+
+
+
+  $( ".fas" ).click(function() {
+    $(this).css('background-color','green');
+  });
 
 
 

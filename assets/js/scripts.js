4e38c1247a0a76ae9a643af3fb83585af8785cd7
nNlson rRmos Lopez <jerunel@hotmail.com>
2021-07-09 22:12:47 -0400

    actualizacion y cambios

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,39 +1,74 @@
 $(document).ready(function () {
-
-console.log("entramos en js");
-
+  console.log("entramos en js");
 
 
-//   Cambiar color mouse pasa sobre y cuando sale de posicion buelve a color blanco
-$(".fas").mouseover(function() {
-    $(this).css('background-color','red');
+  // Funciones
+
+  let jugarcachipun = (numero) => {
+    for (let index = 1; index <= numero; index++) {
+      console.log("siclo for");
+    }
+  }
+
+  // jugada aleatoria computador
+  let jugadacomputador = () => {
+
+let jugada = Math.floor(Math.random() * 3)
+
+if (jugada == 0) return 'fas fa-hand-paper';
+if (jugada == 1) return 'fas fa-hand-rock'; 
+if (jugada == 2) return 'fas fa-hand-scissors'; 
+   console.log("jugada dentro del random :",jugada);
+}
+
+
+
+
+  // main
+
+  var numero = prompt("Ingresar numero de veces que desea jugar")
+
+  console.log(numero);
+
+
+
+
+
+  jugarcachipun(numero);
+  
+  //   Cambiar color mouse pasa sobre y cuando sale de posicion buelve a color blanco
+  $(".fas").mouseover(function () {
+    $(this).css('background-color', 'red');
   });
 
-$(".fas").mouseleave(function() {
-  $(this).css('background-color','white');
+  $(".fas").mouseleave(function () {
+    $(this).css('background-color', 'white');
   });
 
 
 
-  $(".fas").click(function() {
-var mano;
+  $(".fas").click(function () {
+    var mano;
 
-    $(this).css('background-color','green');
+    $(this).css('background-color', 'green');
 
-    document.getElementById("tiempo").style.visibility = "visible";
-    console.log("aparecer");
-    // $("#tiempo").show();
-    setTimeout(function(){
-        $("#tiempo").css('visibility','hidden');
-        console.log("desaparecer")
-    },5000); 
+ 
 
     
-    if (this.className == "fas fa-hand-paper") mano =1;
-    if (this.className == "fas fa-hand-rock") mano =2;
-    if (this.className == "fas fa-hand-scissors") mano =3;
 
-    console.log(this.className+ " " + mano);
+    let papel = "fas fa-hand-paper";
+    let piedra = "fas fa-hand-rock";
+    let tijeras = "fas fa-hand-scissors";
+    let cpu = jugadacomputador();
+
+    let contenidohtml = `<i class="${cpu}"></i>`;
+    
+    console.log(this.className + " " + cpu);
+        
+    document.getElementById("tiempo").innerHTML = contenidohtml;
+     
+   
+  
 
 
   });
